33dbce6aa22508c07187a37e32270411ecb0a6a2
ZjCantarona <74037702+ZjCantarona@users.noreply.github.com>
2023-12-12 00:11:27 +0800

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
-from tensorflow.keras import callbacks,optimizers
-import h5py
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
+import numpy as np
+from PIL import Image, ImageOps
+import cv2
+
+# Define the path to your model file
+MODEL_PATH = "path/to/your/model.h5"
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    h5 = h5py.File(f)
-    return tf.keras.models.load_model(h5)
+    # Load the model using tf.keras.models.load_model
+    return tf.keras.models.load_model(MODEL_PATH)
 
 st.write("""
          # Item Purchase
@@ -17,17 +21,15 @@ st.write("""
 
 file = st.file_uploader("Choose item images from computer", type=["jpg", "png"])
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
 def import_and_predict(image_data, model):
-    size = (224, 224)    
+    size = (224, 224)
     image_object = ImageOps.fit(image_data, size, Image.LANCZOS)
     image_array = np.asarray(image_object)
     image_cv = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
-    img_reshape = image_cv[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    img_reshape = image_cv[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
 
 if file is None:
@@ -35,7 +37,13 @@ if file is None:
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    predictions = import_and_predict(image, load_model())
-    class_names = ['Vegetables','Packages', 'Fruits']
-    string="This image is: "+class_names[np.argmax(predictions)]
-    st.success(string)
+
+    # Check if the model is loaded successfully
+    model = load_model()
+    if model is not None:
+        predictions = import_and_predict(image, model)
+        class_names = ['Vegetables', 'Packages', 'Fruits']
+        string = "This image is: " + class_names[np.argmax(predictions)]
+        st.success(string)
+    else:
+        st.text("Error loading the model. Please check the model path.")
