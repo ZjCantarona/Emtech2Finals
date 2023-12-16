f7b62c8398980e3a45515c38b296a146c3bb7d9d
ZjCantarona <74037702+ZjCantarona@users.noreply.github.com>
2023-12-17 02:02:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 #@st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('model.h5')
+  model=tf.keras.models.load_model('Item.hdf5')
   return model
 model=load_model()
 st.write("""
