268e5be648dea0d5159e62eda79197df2a37d0df
ZjCantarona <74037702+ZjCantarona@users.noreply.github.com>
2023-12-18 22:52:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,68 @@
-import tensorflow as tf
 import streamlit as st
-from PIL import Image, ImageOps
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
 import numpy as np
+from PIL import Image, ImageOps
+import requests
+import io  # Import the standard Python io module
 
+# Direct link to the raw model file on GitHub
+MODEL_URL = "https://github.com/your-username/your-repo/raw/master/path/to/your/model.h5"
+
+@st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('model.h5')
-    return model
-
-model = load_model()
+    try:
+        # Fetch the model from GitHub using requests
+        response = requests.get(MODEL_URL)
+        response.raise_for_status()
+        
+        # Load the model from the content of the response using io.BytesIO
+        model_content = response.content
+        return tf.keras.models.load_model(io.BytesIO(model_content))
+    except Exception as e:
+        st.error(f"Error loading the model: {e}")
+        return None
 
 st.write("""
-# Item Purchase of Group 2
-""")
-file = st.file_uploader("Choose plant photo from computer", type=["jpg", "png"])
+         # Item Purchase
+         """
+        )
+
+file = st.file_uploader("Choose item images from computer", type=["jpg", "png"])
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 def import_and_predict(image_data, model):
-    size = (64, 64)
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
-    return prediction
+    try:
+        size = (224, 224)
+        image_object = ImageOps.fit(image_data, size, Image.LANCZOS)
+        image_array = np.asarray(image_object)
+        img_reshape = image_array[np.newaxis, ...]
+
+        # Display the reshaped image
+        st.image(img_reshape, channels="RGB", use_column_width=True)
+
+        prediction = model.predict(img_reshape)
+        return prediction
+    except Exception as e:
+        st.error(f"Error during prediction: {e}")
+        return None
 
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    class_names = ['Vegetables', 'Packages', 'Fruits']
-    string = "OUTPUT : " + class_names[np.argmax(prediction)]
-    st.success(string)
+    try:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
 
+        # Check if the model is loaded successfully
+        model = load_model()
+        if model is not None:
+            predictions = import_and_predict(image, model)
+            if predictions is not None:
+                class_names = ['Vegetables', 'Packages', 'Fruits']
+                string = "This image is: " + class_names[np.argmax(predictions)]
+                st.success(string)
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
